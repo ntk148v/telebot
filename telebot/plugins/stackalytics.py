77b67cfc8c6c710f5912251e1d090ca3668d4809
Kien Nguyen <kiennt@vn.fujitsu.com>
2017-12-21 13:06:29 +0700

    Update stackalytics plugin

--- telebot/plugins/stackalytics.py
+++ telebot/plugins/stackalytics.py
@@ -1,32 +1,87 @@
-from datetime import datetime
+"""Get report from Stackalytics
+/stackalytics - Get report from Stackalytics
+
+- Just type /stackalytics without any additional options.
+- Firstly, need to upload config file with json format:
+
+For e.x:
+    {
+        "company": "Fujitsu",
+        "members": [
+            "kiennt26",
+            "daidv"
+        ]
+    }
+"""
 import json
-from urllib import request
+import logging
+import urllib.error
+import urllib.request
+
+from telegram import ParseMode
+
+from telebot import utils
+
+LOG = logging.getLogger(__name__)
 
 
-def query(user_id=None, company=None, release='queens'):
+def query(bot, update, user_id=None, company=None):
     """Query to http://stackalytics.com/"""
-    url = 'http://stackalytics.com/api/1.0/contribution?release=queens' . \
-            format(release)
+    url = 'http://stackalytics.com/api/1.0/contribution?'
 
     if company:
-        url += '&company={}' . format(company)
+        url += 'company={}' . format(company)
     if user_id:
         url += '&user_id={}' . format(user_id)
 
-    stats = urllib.request.urlopen(url)
+    LOG.debug('Query url: {}' . format(url))
+
     try:
-        stats = json.loads(stats.read())
+        stats = urllib.request.urlopen(url)
+        stats = json.loads(stats.read().decode())
     except ValueError as e:
-        LOG.error('Error when execute query: {}' . format(e))
+        msg = 'Error with JSON format: {}' . format(e)
+        utils.handle_error(LOG, bot, update.message.chat_id, msg)
+        return None
+    except urllib.error.HTTPError as e:
+        msg = 'Error when query member {}\'s stats: {}' . format(user_id, e)
+        utils.handle_error(LOG, bot, update.message.chat_id, msg)
         return None
     patches = stats['contribution']['patch_set_count']
-    commits = stats['contribution']['commit_set_count']
+    commits = stats['contribution']['commit_count']
     reviews = \
-            stats['contribution']['marks']['1'] + \
-            stats['contribution']['marks']['-1'] + \
-            stats['contribution']['marks']['2'] + \
-            stats['contribution']['marks']['-2']
+        stats['contribution']['marks']['1'] + \
+        stats['contribution']['marks']['-1'] + \
+        stats['contribution']['marks']['2'] + \
+        stats['contribution']['marks']['-2']
     return (patches, commits, reviews)
 
+
 def handle(bot, update):
-    pass
+    try:
+        config = json.load(open('/tmp/stackalyticsconfig.json'))
+    except FileNotFoundError:
+        msg = 'Config file doesn\' exist! Type /hep stackalytics again to \
+               check usage!'
+        utils.handle_error(LOG, bot, update.message.chat_id, msg)
+        return
+    except json.decoder.JSONDecodeError:
+        msg = 'Wrong format! Type /hep stackalytics again to \
+              check right format!'
+        utils.handle_error(LOG, bot, update.message.chat_id, msg)
+        return
+
+    text = 'Report:\n'
+    template = '- Member `{}`: `{}` patches, `{}` commits, `{}` reviews.\n'
+    for member in config['members']:
+        results = query(bot, update,
+                        user_id=member,
+                        company=config['company'])
+        if not results:
+            continue
+
+        patches, commits, reviews = results
+        text += template.format(member, patches, commits, reviews)
+    bot.send_message(chat_id=update.message.chat_id,
+                     text=text,
+                     parse_mode=ParseMode.MARKDOWN)
