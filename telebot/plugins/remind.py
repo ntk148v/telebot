72d7c96b0db39162b9ceb53794aa0be2ea28419b
Kien Nguyen <kiennt@vn.fujitsu.com>
2017-12-22 10:01:42 +0700

    Update remind plugin - make it works

--- telebot/plugins/remind.py
+++ telebot/plugins/remind.py
@@ -1,28 +1,39 @@
 """Remind everyone in team to send report
-/remind thing_to_remind - Run at xx:xx everyday!
+/remind hour:minute reminder - Remind at hour:minute everyday!
 """
 import datetime
 
+from telebot import emojies
 
-def do_remind(bot, job, reminder):
-    bot.send_message(job.context, text=reminder)
+
+def do_remind(bot, job):
+    reminder = '{} `{}` {}' . format(
+        emojies.bell, job.context['reminder'],
+        emojies.bell)
+
+    chat_id = job.context['chat_id']
+    bot.send_message(chat_id=chat_id, text=reminder)
     return
 
 
 def handle(bot, update, args, job_queue, chat_data):
     """Remind something at xx:xx everyday!"""
     chat_id = update.message.chat_id
+    context = {}
 
     try:
-        reminder = args[0]
+        hour, minute = map(int, args.pop(0).split(':'))
+        reminder = ' '.join(args)
+        context['chat_id'] = chat_id
+        context['reminder'] = reminder
 
         # Add job to queue, run daily
-        job = job_queue.run_daily(do_remind, reminder,
-                                  time=datetime.time('14:32'),
-                                  context=chat_id)
+        job = job_queue.run_daily(do_remind,
+                                  time=datetime.time(hour=hour, minute=minute),
+                                  context=context)
         chat_data['job'] = job
 
         update.message.reply_text('Timer successfully set!')
 
     except(IndexError, ValueError):
-        update.message.reply_text('Usage: /remind thing_to_remind')
+        update.message.reply_text('Usage: /remind hour:minute reminder')
